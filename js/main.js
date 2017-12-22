b0d41231653c734da01a4075b880ba95caaec420
Evgenii Klimov <disturh@gmail.com>
2017-12-22 22:01:48 +0300

    downloading images from Blogger

--- js/main.js
+++ js/main.js
@@ -1,41 +1,34 @@
 window.G = {
-    isMobile: window.innerWidth < 768 ? true : false,
-
     pages: {
-        'drawings': {
+        'drawing': {
             type: 'gallery',
         },
-        'paintings': {
+        'painting': {
             type: 'gallery',
         },
         'about': {
             type: 'static',
         },
     },
+    currentPage: 'drawing',
+    works: {},
 };
 
 ;(function start() {
-    initMenu();
-    initPages();
     checkForMobile();
+    initPages();
+    getData();
 })();
 
-function initMenu() {
-    $('.menu').on('click', 'div', toggleMenu);
+function checkForMobile() {
+    G.isMobile = window.innerWidth < 768 ? true : false;
 
-    function toggleMenu() {
-        console.log(this);
-        var $menu__btn = $(this);
-        var $menu = $menu__btn.parents('.menu');
-        var classOpened = 'menu-opened';
-
-        if($menu.hasClass(classOpened)) {
-            $menu.removeClass(classOpened);
-        } else {
-            $menu.addClass(classOpened);
-        }
-    }
+    $(window).resize(function () {
+        G.isMobile = window.innerWidth < 768 ? true : false;
+    });
 }
+
+
 
 function initPages() {
     var loaders = {
@@ -58,25 +51,51 @@ function initPages() {
     }
 }
 
-function checkForMobile() {
-    $(window).resize(function () {
-        G.isMobile = window.innerWidth < 768 ? true : false;
-    });
+
+
+function getData() {
+    $.getJSON(
+        "https://www.googleapis.com/blogger/v3/blogs/327656489361647821/posts",
+        {
+            key: 'AIzaSyA-T9NRjIXJMQHWuf4TEZfAoBG9sfvarQg',
+        },
+        handle
+    );
+    function handle(data) {
+
+
+
+        $.each(data.items, function (i, post) {
+            var $content = $('<div>' + post.content + '</div>');
+            var imgSrc = $content.find('img')[0].src;
+            var imgSrcOriginal = $content.find('a')[0].href;
+
+            var work = {
+                src: imgSrc,
+                srcOrig: imgSrcOriginal,
+            };
+
+            var label = post.labels;
+            if (label) {
+                label = label[0];
+
+                // debugger;
+                G.works[label] = G.works[label] ? G.works[label] : [];
+                G.works[label].push(work);
+            }
+
+        });
+
+        console.log(G.works);
+
+        var content = '';
+        $.each (G.works[G.currentPage], function (i, work) {
+            content += '<img src="'+ work.src +'">';
+        });
+
+
+        $('.gallery__room.mini').html(content);
+    }
 }
 
 
-
-$.getJSON(
-    "https://www.googleapis.com/blogger/v3/blogs/2162001888846871697/posts",
-    {
-        key: 'AIzaSyA-T9NRjIXJMQHWuf4TEZfAoBG9sfvarQg',
-    },
-    handle
-);
-function handle(data) {
-    console.log(data);
-
-    var $description = $(data.items[0].content);
-
-    $('.gallery__room.mini').html($description);
-}
