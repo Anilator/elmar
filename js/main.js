2452c5af4cb8689e70b23ade0c76ff82c14e834e
Evgenii Klimov <disturh@gmail.com>
2017-12-25 20:31:42 +0300

    image resizing test

--- js/main.js
+++ js/main.js
@@ -64,41 +64,53 @@ function getData() {
     function handle(data) {
 
 
+        function resizeTo(src, size) {
+            var srcSplitted = src.split('/');
+
+            srcSplitted[7] = 's' + size;
+
+            return srcSplitted.join('/');
+        }
+
+
 
         $.each(data.items, function (i, post) {
             var $content = $('<div>' + post.content + '</div>');
-            var imgSrc = $content.find('img')[0].src;
-            var imgSrcOriginal = $content.find('a')[0].href;
+            var src = $content.find('a')[0].href;
+            // debugger;
+
 
             var work = {
-                src: imgSrc,
-                srcOrig: imgSrcOriginal,
+                src: src,
+                srcThumb: G.isMobile ? resizeTo(src, 300) : resizeTo(src, 500),
             };
 
             var labels = post.labels;
             if (labels) {
                 label = labels[0];
                 if (label[0] == '#') {
-                    G.background = label;
+                    G.backgroundColor = label;
+                    G.heroImage = G.isMobile ? resizeTo(src, 500) : src;
                     label = labels[1];
+                } else {
+                    G.works[label] = G.works[label] ? G.works[label] : [];
+                    G.works[label].push(work);
                 }
-
-                // debugger;
-                G.works[label] = G.works[label] ? G.works[label] : [];
-                G.works[label].push(work);
             }
-
         });
 
-        console.log(G.works);
 
-        var content = '';
+
+
+        // console.log(G.works);
+
+        var content = '<img class="gallery__hero" src="'+ G.heroImage +'">';
+
         $.each (G.works[G.currentPage], function (i, work) {
-            content += '<img src="'+ work.src +'">';
+            content += '<img class="gallery__img" src="'+ work.srcThumb +'">';
         });
 
-
-        $('.gallery__room.mini').html(content).css('background', G.background);
+        $('.gallery__room.mini').html(content).css('background', G.backgroundColor);
     }
 }
 
