2940848ea4a81e859a944fd02b5222cd05c9d638
Evgenii Klimov <disturh@gmail.com>
2017-12-27 20:15:32 +0300

    zoomed image switching test

--- js/gallery.js
+++ js/gallery.js
@@ -1,18 +1,56 @@
 ;(function start() {
+    var fullWidth = window.innerWidth;
+    var fullHeight = window.innerHeight;
+    var zoomedImgSize = Math.max(fullWidth, fullHeight) * 2;
 
     window.G = readStorage();
-
     if (window.G) {
-        var fullWidth = window.innerWidth;
-        var fullHeight = window.innerHeight;
-        var zoomedImgSize = Math.max(fullWidth, fullHeight) * 2;
+
+        updateControls();
+        showZoomedImg();
+
+        $('.controls').on('click', 'i', switchImg);
+    }
+
+    function showZoomedImg() {
 
         var activeWorkSrc = G.works[G.activePage][G.activeWork].src;
         var src = changeImgSize(activeWorkSrc, zoomedImgSize);
 
-        content = '<img src="'+ src +'">';
-        $('.gallery').html(content);
+        var content = '<img src="'+ src +'">';
+        $('.gallery').html(content).css('background', G.backgroundColor);
     }
+
+    function switchImg(e) {
+        var $btn = $(e.target);
+        var direction = $btn.data('dir');
+
+        if (direction) { // right
+            if (G.activeWork < G.works[G.activePage].length - 1)
+                G.activeWork++;
+        } else { // left
+            if (G.activeWork)
+                G.activeWork--;
+        }
+
+        updateControls();
+        showZoomedImg();
+    }
+
+    function updateControls() {
+        var $left = $('.controls .left').css('');
+        var $right = $('.controls .right').css('');
+        if (G.activeWork) {
+            if (G.activeWork >= G.works[G.activePage].length - 1)
+                $right.css('display: none');
+        } else {
+            $left.css('display: none');
+        }
+    }
+
+
+
+
 
     function readStorage() {
         var data = {};
